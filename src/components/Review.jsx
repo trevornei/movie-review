295e3c8ed63fd681edc389b9a88d0b073a6ef7d0
trevornei <117675657+trevornei@users.noreply.github.com>
2023-09-15 14:04:07 -0600

    Styled the form components

--- src/components/Review.jsx
+++ src/components/Review.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Review({ reviewData }) {
     return (
-        <div className="flex flex-col items-center justify-center">
+        <div className="flex flex-col items-center justify-center border-4 border-primary rounded-xl p-4 m-2 shadow-limez shadow-lg">
             <h3 className="font-arsenica text-2xl text-limez">Review</h3>
             <p className="font-moret text-white">"{reviewData.opinion}"</p>
             <p className="font-moret text-black">- {reviewData.name}</p>
--- src/components/ReviewForm.jsx
+++ src/components/ReviewForm.jsx
@@ -6,10 +6,20 @@ export default function ReviewForm({  }) {
 //rerender on the dom, whenever the reivews array updates
 
   return (
-    <div>
-      <form action="" className="">
-        <label htmlFor="" className=""></label>
-        <input type="text" className="" />
+    <div className='flex flex-col items-center justify-center mt-6'>
+      <h3 className='font-arsenica text-2xl tracking-widest text-limez'>Leave a Review!</h3>
+      <form action="" className="flex flex-col items-center justify-center">
+        <div className="m-2 w-full h-auto">
+          <label htmlFor="" className=""></label>
+          <input type="text" className="rounded-lg w-full placeholder:text-primary placeholder:font-moret placeholder:tracking-wide text-secondary font-moret tracking-wide " placeholder='Full Name'/>
+        </div>
+        <div className="m-2 w-full h-auto">
+          <label htmlFor="" className=""></label>
+          <input type="text" className="rounded-lg w-full h-6 placeholder:text-primary placeholder:font-moret placeholder:tracking-wide text-secondary font-moret tracking-wide " placeholder='Review'/>
+        </div>
+        <button type="submit" className='bg-primary rounded-xl p-1 m-2 shadow-primary shadow-lg'>
+          <p className='font-moret text-xl tracking-widest text-limez'>Submit</p>
+        </button>
       </form>
     </div>
   )
